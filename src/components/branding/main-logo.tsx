17331b279e2009fe79197fe2f4b9a6249bb7c4bf
phalla.doll <phalla.doll@gmail.com>
2025-09-14 14:58:00 +0700

    Add logo SVG files for dark and white themes, and implement MainLogo component for responsive branding

---
+++ src/components/branding/main-logo.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export function MainLogo() {
+    return (
+        <>
+            <div className="hidden sm:block">
+                <Image src="/logos/logo-white.svg" className="hidden dark:block" alt="TeamTag Logo White" width={100} height={100} />
+                <Image src="/logos/logo-dark.svg" className="dark:hidden" alt="TeamTag Logo Dark" width={100} height={100} />
+            </div>
+            <div className="block sm:hidden">
+                <Image src="/logos/single-logo-white.svg" className="hidden dark:block" alt="TeamTag Logo White" width={24} height={24} />
+                <Image src="/logos/single-logo-dark.svg" className="dark:hidden" alt="TeamTag Logo Dark" width={24} height={24} />
+            </div>
+        </>
+    )
+}
