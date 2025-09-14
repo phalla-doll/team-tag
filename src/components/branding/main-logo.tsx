f4a892eafcbb5fe579cf975fcdb1dfa2fe4b7543
phalla.doll <phalla.doll@gmail.com>
2025-09-14 15:05:41 +0700

    Wrap MainLogo component in a Link to enable navigation to the homepage

--- src/components/branding/main-logo.tsx
+++ src/components/branding/main-logo.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export function MainLogo() {
     return (
-        <>
+        <Link href="/">
             <div className="hidden sm:block">
                 <Image src="/logos/logo-white.svg" className="hidden dark:block" alt="TeamTag Logo White" width={100} height={100} />
                 <Image src="/logos/logo-dark.svg" className="dark:hidden" alt="TeamTag Logo Dark" width={100} height={100} />
@@ -11,6 +12,6 @@ export function MainLogo() {
                 <Image src="/logos/single-logo-white.svg" className="hidden dark:block" alt="TeamTag Logo White" width={24} height={24} />
                 <Image src="/logos/single-logo-dark.svg" className="dark:hidden" alt="TeamTag Logo Dark" width={24} height={24} />
             </div>
-        </>
+        </Link>
     )
 }
